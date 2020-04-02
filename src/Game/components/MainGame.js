dd235790f85d159b8d85b5a5ae2416e4363ec928
Abdul Rashid <ar1242112@gmail.com>
2020-04-03 00:12:26 +0530

    fixed allingment

--- src/Game/components/MainGame.js
+++ src/Game/components/MainGame.js
@@ -7,74 +7,72 @@ import { Row, Col } from "react-bootstrap";
 import { ALL_CARDS, FINAL_ASNWER, LEVELS, START_CARDS } from "../Data";
 
 export default function MainGame() {
-	const roomDetails = {
-		name: "Office",
-		description:
-			"In this adventure, you are a top ranked detective. You are extremely close to solving a high-class murder case. A tip leads you to the office space of a suspect. The clue that you want to gather might be the last missing puzzle piece to connect the dots. Without any prior preparation you rush to the suspect’s office. Its late in the night, so you have managed to sneak in without being noticed by the cameras and security guards. You enter the office room AND slam, the door shut on its own. You try to open it again but it won’t open. Seem like you have fallen into a trap. You have limited time to escape the room. GOOD LUCK. ",
-		imgURL: "office.jpg"
-	};
+  const roomDetails = {
+    name: "Office",
+    description:
+      "In this adventure, you are a top ranked detective. You are extremely close to solving a high-class murder case. A tip leads you to the office space of a suspect. The clue that you want to gather might be the last missing puzzle piece to connect the dots. Without any prior preparation you rush to the suspect’s office. Its late in the night, so you have managed to sneak in without being noticed by the cameras and security guards. You enter the office room AND slam, the door shut on its own. You try to open it again but it won’t open. Seem like you have fallen into a trap. You have limited time to escape the room. GOOD LUCK. ",
+    imgURL: "office.jpg"
+  };
 
-	const [activeCards, setActiveCards] = useState(
-		START_CARDS.map(i => ALL_CARDS[i])
-	);
-	const [penalty, setPenalty] = useState(false);
-	const [roundAnswers, setroundAnswers] = useState(LEVELS);
+  const [activeCards, setActiveCards] = useState(
+    START_CARDS.map(i => ALL_CARDS[i])
+  );
+  const [penalty, setPenalty] = useState(false);
+  const [roundAnswers, setroundAnswers] = useState(LEVELS);
 
-	function verifyCards(number, finalNumber) {
-		if (finalNumber !== undefined) {
-			if (FINAL_ASNWER === finalNumber.toString()) {
-				alert("You WIN");
-			} else {
-				alert("You loose");
-			}
-		} else {
-			if (number.toString() === roundAnswers[0].key) {
-				setActiveCards([
-					...activeCards,
-					...roundAnswers[0].reveled_cards.map(i => ALL_CARDS[i])
-				]);
-				roundAnswers.splice(0, 1);
-				setroundAnswers(roundAnswers);
-			} else {
-				setPenalty(true);
-			}
-		}
-	}
+  function verifyCards(number, finalNumber) {
+    if (finalNumber !== undefined) {
+      if (FINAL_ASNWER === finalNumber.toString()) {
+        alert("You WIN");
+      } else {
+        alert("You loose");
+      }
+    } else {
+      if (number.toString() === roundAnswers[0].key) {
+        setActiveCards([
+          ...activeCards,
+          ...roundAnswers[0].reveled_cards.map(i => ALL_CARDS[i])
+        ]);
+        roundAnswers.splice(0, 1);
+        setroundAnswers(roundAnswers);
+      } else {
+        setPenalty(true);
+      }
+    }
+  }
 
-	function discard(cardNumber) {
-		console.log(cardNumber);
-		console.log(activeCards);
-		for (let i = 0; i < activeCards.length; i++) {
-			if (activeCards[i].number === cardNumber) {
-				activeCards.splice(i, 1);
-			}
-		}
-		const removed = [];
-		Array.prototype.push.apply(removed, activeCards);
-		setActiveCards(removed);
-	}
-	return (
-		<>
-			<Timer penalty={penalty} setPenalty={setPenalty}></Timer>
-			<RoomCard details={roomDetails}></RoomCard>
+  function discard(cardNumber) {
+    console.log(cardNumber);
+    console.log(activeCards);
+    for (let i = 0; i < activeCards.length; i++) {
+      if (activeCards[i].number === cardNumber) {
+        activeCards.splice(i, 1);
+      }
+    }
+    const removed = [];
+    Array.prototype.push.apply(removed, activeCards);
+    setActiveCards(removed);
+  }
+  return (
+    <>
+      <Timer penalty={penalty} setPenalty={setPenalty}></Timer>
+      <RoomCard details={roomDetails}></RoomCard>
 
-			<Row>
-				{activeCards.map(c => (
-					<>
-						<Col md={3}>
-							<ObjectCard discard={discard} card={c}></ObjectCard>
-						</Col>
-					</>
-				))}
-			</Row>
+      <Row>
+        {activeCards.map(c => (
+          <Col md={3} style={{ paddingBottom: "1rem" }}>
+            <ObjectCard discard={discard} card={c}></ObjectCard>
+          </Col>
+        ))}
+      </Row>
 
-			<br />
+      <br />
 
-			<Row>
-				<Col md={6}>
-					<SubmissionModal verifyCards={verifyCards}></SubmissionModal>
-				</Col>
-			</Row>
-		</>
-	);
+      <Row>
+        <Col md={6}>
+          <SubmissionModal verifyCards={verifyCards}></SubmissionModal>
+        </Col>
+      </Row>
+    </>
+  );
 }
--- src/Game/components/ObjectCard/ObjectCard.js
+++ src/Game/components/ObjectCard/ObjectCard.js
@@ -4,59 +4,60 @@ import "./ObjectCard.css";
 import { Row, Col } from "react-bootstrap";
 
 export default function ObjectCard(props) {
-	const details = props.card;
-	const discard = props.discard;
+  const details = props.card;
+  const discard = props.discard;
 
-	return (
-		<Card
-			className="card"
-			style={{
-				marginBottom: "12px",
-				backgroundColor: "#A9E4EA",
-				textAlign: "left"
-			}}
-			//	onClick={() => selectCard(details.number)}
-		>
-			<Card.Img
-				class="img-fluid"
-				variant="top"
-				src={`${window.location.origin}/assets/images/${details.imgURL}`}
-			/>
-			<Card.Body style={{ backgroundColor: "#242323", color: "#A9E4EA	" }}>
-				<Card.Title style={{ fontFamily: "'Merienda One', cursive" }}>
-					{details.name}
-				</Card.Title>
-				<Row>
-					<Col md={10}>
-						<Card.Text
-							style={{
-								fontSize: "12px",
-								color: "#A9E4EA",
-								fontFamily: "'Comfortaa', cursive"
-							}}
-						>
-							{details.text}
-						</Card.Text>
-					</Col>
+  return (
+    <Card
+      className="h-100"
+      style={{
+        backgroundColor: "#A9E4EA",
+        textAlign: "left"
+      }}
+      //	onClick={() => selectCard(details.number)}
+    >
+      <Card.Img
+        class="img-fluid"
+        variant="top"
+        src={`${window.location.origin}/assets/images/${details.imgURL}`}
+      />
+      <Card.Body style={{ backgroundColor: "#242323", color: "#A9E4EA	" }}>
+        <Card.Title style={{ fontFamily: "'Merienda One', cursive" }}>
+          {details.name}
+        </Card.Title>
+        <Row>
+          <Col md={10}>
+            <Card.Text
+              style={{
+                fontSize: "12px",
+                color: "#A9E4EA",
+                fontFamily: "'Comfortaa', cursive"
+              }}
+            >
+              {details.text}
+            </Card.Text>
+          </Col>
 
-					<Col md={1}>
-						<button
-							className="btn"
-							style={{
-								backgroundColor: "#A9E4EA",
-								fontSize: "1.3rem",
-								boxShadow: "0px 1px 10px #999"
-							}}
-						>
-							{details.number}
-						</button>
-					</Col>
-				</Row>
-				<br />
-				<Button onClick={() => discard(details.number)} variant="danger">
-					Discard
-				</Button>
-			</Card.Body>
-		</Card>
-	);
+          <Col md={1}>
+            <button
+              className="btn"
+              style={{
+                backgroundColor: "#A9E4EA",
+                fontSize: "1.3rem",
+                boxShadow: "0px 1px 10px #999"
+              }}
+            >
+              {details.number}
+            </button>
+          </Col>
+        </Row>
+        <br />
+      </Card.Body>
+      <Card.Footer style={{ backgroundColor: "#242323", color: "#A9E4EA	" }}>
+        <Button onClick={() => discard(details.number)} variant="danger">
+          Discard
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
 }
