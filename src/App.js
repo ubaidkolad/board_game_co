b8b8adfa588bf9e90a6a198b763e9bea9f7fa5cf
Abdul Rashid <ar1242112@gmail.com>
2020-04-08 18:04:40 +0530

    added dyanamic routing

--- src/App.js
+++ src/App.js
@@ -8,38 +8,38 @@ import Login from "./Game/components/Login/Login";
 import InstructionsPage from "./Game/components/Instructions/InstructionsPage";
 import VerifyRoom from "./Game/components/VerifyRoom";
 import ProtectedRoute from "./Game/components/ProtectedRoute";
-import { setGlobal } from "reactn";
+// import { setGlobal } from "reactn";
 
 function App() {
-	const see = (routerProps) => {
-		console.log(routerProps);
-		console.log("ok");
-	};
-	return (
-		<div
-			className="App"
-			style={{
-				backgroundImage: `url(${window.location.origin}/assets/bg.jpg)`,
-				backgroundSize: "cover",
-				overflow: "hidden",
-			}}
-		>
-			<Nav />
-			<Router>
-				<Switch>
-					<Route exact path="/" component={Login} />
+  const see = (routerProps) => {
+    console.log(routerProps);
+    console.log("ok");
+  };
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${window.location.origin}/assets/bg.jpg)`,
+        backgroundSize: "cover",
+        overflow: "hidden",
+      }}
+    >
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/gameroom/:id" component={Login} />
 
-					<Route path="/instructions">
-						<InstructionsPage />
-					</Route>
-					<ProtectedRoute path="/game" component={Game} />
+          <Route path="/instructions">
+            <InstructionsPage />
+          </Route>
+          <ProtectedRoute path="/game" component={Game} />
 
-					<ProtectedRoute path="/endgame" component={EndGame} />
-					<Route path="*" component={() => "404 not found"} />
-				</Switch>
-			</Router>
-		</div>
-	);
+          <ProtectedRoute path="/endgame" component={EndGame} />
+          <Route path="*" component={() => "404 not found"} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Game/components/Login/Login.js
+++ src/Game/components/Login/Login.js
@@ -1,134 +1,137 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
 import Cookies from "js-cookie";
 import { url } from "../url";
 import Spinner from "react-bootstrap/Spinner";
-import { withRouter } from "react-router-dom";
+import { withRouter, useRouteMatch } from "react-router-dom";
 
 function Login(props) {
-	Cookies.remove("email");
-	Cookies.remove("verified");
-	//console.log(props.location.state.error);
-	const [verification, setVerification] = useState(false);
-	const [resp, setResp] = useState();
-	const [email, setEmail] = useState();
+  Cookies.remove("email");
+  Cookies.remove("verified");
 
-	function onChangeEmail(e) {
-		setEmail(e.target.value);
-	}
+  const [verification, setVerification] = useState(false);
+  const [resp, setResp] = useState();
+  const [email, setEmail] = useState();
+  const [id, setid] = useState(useRouteMatch("/gameroom/:id").params.id);
 
-	function validateEmail(e) {
-		e.preventDefault();
-		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  console.log(useRouteMatch("/gameroom/:id").params.id);
 
-		if (reg.test(email) == false) {
-			alert("Invalid Email Address");
-			return false;
-		}
+  function onChangeEmail(e) {
+    setEmail(e.target.value);
+  }
 
-		return true;
-	}
+  function validateEmail(e) {
+    e.preventDefault();
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-	function authentication(e) {
-		console.log(email);
-		if (email === undefined) {
-			e.preventDefault();
-			alert("Please enter your email address");
-		} else {
-			e.preventDefault();
-			setResp(true);
-			let formData = new FormData();
-			formData.append("email", email);
+    if (reg.test(email) == false) {
+      alert("Invalid Email Address");
+      return false;
+    }
 
-			axios
-				.post(`${url}/user/verify/test-test-1586344743`, formData)
-				.then((response) => handleResponse(response))
-				.catch((err) => {
-					setResp(false);
-					alert(err);
-				});
-		}
-	}
+    return true;
+  }
 
-	function handleResponse(response) {
-		if (response.status === 200) {
-			setResp(false);
+  function authentication(e) {
+    console.log(email);
+    if (email === undefined) {
+      e.preventDefault();
+      alert("Please enter your email address");
+    } else {
+      e.preventDefault();
+      setResp(true);
+      let formData = new FormData();
+      formData.append("email", email);
 
-			setVerification(true);
+      axios
+        .post(`${url}/user/verify/${id}`, formData)
+        .then((response) => handleResponse(response))
+        .catch((err) => {
+          setResp(false);
+          alert(err);
+        });
+    }
+  }
 
-			Cookies.set("email", email);
-			Cookies.set("verified", "true");
-			//	alert(response.data.msg);
-		} else {
-			alert("not authorized");
-		}
-	}
+  function handleResponse(response) {
+    if (response.status === 200) {
+      setResp(false);
 
-	return (
-		<>
-			<div
-				class="container"
-				style={{ paddingLeft: "25rem", paddingTop: "5rem", minHeight: "100vh" }}
-			>
-				<div id="first">
-					<div class="myform form ">
-						<div class="logo mb-3">
-							<div class="col-md-12 text-center">
-								<h1>Login</h1>
-							</div>
-						</div>
-						<form>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Email address</label>
-								<input
-									type="email"
-									name="email"
-									class="form-control"
-									id="email"
-									aria-describedby="emailHelp"
-									placeholder="Enter email"
-									onBlur={validateEmail}
-									onChange={onChangeEmail}
-									required
-								/>
-							</div>
-							{props.location.state === undefined ? (
-								<> </>
-							) : (
-								<>
-									{" "}
-									<p style={{ color: "red", marginLeft: "5rem" }}>
-										{props.location.state.error}
-									</p>{" "}
-								</>
-							)}
+      setVerification(true);
 
-							<div class="col-md-12 text-center ">
-								<input
-									type="submit"
-									value="Login"
-									class=" btn btn-block mybtn btn-primary tx-tfm"
-									onClick={authentication}
-								/>
-							</div>
-						</form>
-					</div>
-				</div>
-				{resp ? (
-					<h7>
-						<Spinner animation="border" variant="primary" />
-						<br />
-						Verifying, Please wait...
-					</h7>
-				) : (
-					<></>
-				)}
-			</div>
-			{verification ? <Redirect to="/instructions" /> : <> </>}
-		</>
-	);
+      Cookies.set("email", email);
+      Cookies.set("verified", "true");
+      //	alert(response.data.msg);
+    } else {
+      alert("not authorized");
+    }
+  }
+
+  return (
+    <>
+      <div
+        class="container"
+        style={{ paddingLeft: "25rem", paddingTop: "5rem", minHeight: "100vh" }}
+      >
+        <div id="first">
+          <div class="myform form ">
+            <div class="logo mb-3">
+              <div class="col-md-12 text-center">
+                <h1>Login</h1>
+              </div>
+            </div>
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  class="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  onBlur={validateEmail}
+                  onChange={onChangeEmail}
+                  required
+                />
+              </div>
+              {props.location.state === undefined ? (
+                <> </>
+              ) : (
+                <>
+                  {" "}
+                  <p style={{ color: "red", marginLeft: "5rem" }}>
+                    {props.location.state.error}
+                  </p>{" "}
+                </>
+              )}
+
+              <div class="col-md-12 text-center ">
+                <input
+                  type="submit"
+                  value="Login"
+                  class=" btn btn-block mybtn btn-primary tx-tfm"
+                  onClick={authentication}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+        {resp ? (
+          <h7>
+            <Spinner animation="border" variant="primary" />
+            <br />
+            Verifying, Please wait...
+          </h7>
+        ) : (
+          <></>
+        )}
+      </div>
+      {verification ? <Redirect to="/instructions" /> : <> </>}
+    </>
+  );
 }
 
 export default withRouter(Login);
